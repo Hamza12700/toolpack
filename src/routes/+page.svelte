654ff8cdc005c201730d0c58f6570dab7e6df595
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-01-31 21:00:29 +0400

    added: og-meta-generator tool card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
+<script lang="ts">
+	import ToolCard from "$lib/Tool_Card.svelte";
+</script>
 
+<div class="mt-4 flex-wrap justify-between gap-10 flex">
+	<ToolCard title="OpenGraph meta generator" link="/og-meta-generator" description="Generate open-graph and socials html meta tags for your website" />
+</div>
