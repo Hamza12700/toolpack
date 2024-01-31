77072dc57ec9cd25581e6580f232ca86c1b67aba
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-01-31 19:25:19 +0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.pcss';
+	import Icon from "@iconify/svelte"
+</script>
+
+<title>Tooly</title>
+<nav class="p-4 flex items-center gap-2">
+	<Icon class="text-4xl" icon="solar:box-bold" />
+	<a class="font-bold text-4xl" href="/">Tooly</a>
+</nav>
+
+<main class="px-4">
+	<slot />
+</main>
