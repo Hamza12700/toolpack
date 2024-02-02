c8b8be1113cfd00e683444e5d95f53a2643692dd
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-02 17:18:05 +0400

    tweaked: styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,9 @@
 </script>
 
 <title>Tooly</title>
-<nav class="py-4 px-8 flex items-center gap-2">
-	<Icon class="text-4xl" icon="solar:box-bold" />
-	<a class="font-bold text-4xl" href="/">Tooly</a>
+<nav class="flex items-center gap-2 px-8 py-4">
+  <Icon class="text-4xl" icon="solar:box-bold" />
+  <a class="text-4xl font-bold" href="/">Tooly</a>
 </nav>
 
 <main class="px-8">
