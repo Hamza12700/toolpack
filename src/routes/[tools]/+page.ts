d9382c6f8d6547b017756fdd0c436610ef70336a
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-02 19:36:25 +0400

    feat: returing the name of the component

--- src/routes/[tools]/+page.ts
+++ src/routes/[tools]/+page.ts
@@ -4,15 +4,18 @@ import type { PageLoad } from "./$types"
 export const load: PageLoad = ({params}) => {
   let title = ""
   let description = ""
+  let component = ""
   tools.forEach(x => {
     if (x.link === "/"+params.tools) {
       title = x.title
       description = x.description
+      component = x.component
     }
   })
 
   return {
     title: title,
-    description: description
+    description: description,
+    component: component
   }
 }
