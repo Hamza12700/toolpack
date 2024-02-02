93f58877e3637fbd941b35154b410320d0c0b4a5
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-02 19:35:36 +0400

    added: `component: string` in Tools type

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -2,7 +2,8 @@ type Tools = {
 	icon_name: string,
 	title: string,
 	description: string,
-	link: string
+	link: string,
+	component: string
 }
 
 export const tools: Tools[] = [
@@ -10,6 +11,7 @@ export const tools: Tools[] = [
 		title: "Token Generator",
 		icon_name: "game-icons:power-generator",
 		description: "Generate random string with the chars you want, uppercase or lowercase letters, numbers and/or symbols",
-		link: "/token-generator"
+		link: "/token-generator",
+		component: "TokenGenerator"
 	}
 ]
