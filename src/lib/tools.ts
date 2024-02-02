0b649659242b0dd97d780cad41cd9513543e0369
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-02 17:21:19 +0400

    feat: tools properties

---
+++ src/lib/tools.ts
@@ -0,0 +1,15 @@
+type Tools = {
+	icon_name: string,
+	title: string,
+	description: string,
+	link: string
+}
+
+export const tools: Tools[] = [
+	{
+		title: "Token Generator",
+		icon_name: "game-icons:power-generator",
+		description: "Generate random string with the chars you want, uppercase or lowercase letters, numbers and/or symbols",
+		link: "/token-generator"
+	}
+]
