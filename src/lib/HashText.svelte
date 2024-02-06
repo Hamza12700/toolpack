33cdc4f5ab495d5e4a0ed16402cc8ca81ac5339f
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-06 20:07:56 +0400

    feat: change the colors to match the dark theme

--- src/lib/HashText.svelte
+++ src/lib/HashText.svelte
@@ -27,36 +27,36 @@
   }
 </script>
 
-<form class="border-b-2 pb-4">
+<form class="border-b-2 border-white/10 pb-4">
   <label class="font-mono" for="hashtext"> Your text to hash: </label>
   <textarea
     bind:value={input}
-    class="mt-1 h-40 w-full rounded-sm bg-black/20 p-2 font-mono"
+    class="mt-1 h-40 w-full rounded-sm bg-white/10 p-2 font-mono"
   ></textarea>
 </form>
 
 <div class="mt-4 flex flex-col justify-start">
   <div class="flex items-center">
-    <span class="mr-3 w-20 rounded-sm bg-black/30 p-1 font-bold">MD5</span>
-    <p class="w-full overflow-scroll rounded-sm bg-black/30 p-1 font-mono">
+    <span class="mr-3 w-20 rounded-sm bg-white/10 p-1 font-bold">MD5</span>
+    <p class="w-full overflow-scroll rounded-sm bg-white/10 p-1 font-mono">
       {hashes.md5}
     </p>
   </div>
   <div class="mt-2 flex items-center">
-    <span class="mr-3 w-20 rounded-sm bg-black/30 p-1 font-bold">SHA1</span>
-    <p class="w-full overflow-scroll rounded-sm bg-black/30 p-1 font-mono">
+    <span class="mr-3 w-20 rounded-sm bg-white/10 p-1 font-bold">SHA1</span>
+    <p class="w-full overflow-scroll rounded-sm bg-white/10 p-1 font-mono">
       {hashes.sha1}
     </p>
   </div>
   <div class="mt-2 flex items-center">
-    <span class="mr-3 w-20 rounded-sm bg-black/30 p-1 font-bold">SHA224</span>
-    <p class="w-full overflow-scroll rounded-sm bg-black/30 p-1 font-mono">
+    <span class="mr-3 w-20 rounded-sm bg-white/10 p-1 font-bold">SHA224</span>
+    <p class="w-full overflow-scroll rounded-sm bg-white/10 p-1 font-mono">
       {hashes.sha224}
     </p>
   </div>
   <div class="mt-2 flex items-center">
-    <span class="mr-3 w-20 rounded-sm bg-black/30 p-1 font-bold">SHA256</span>
-    <p class="w-full overflow-scroll rounded-sm bg-black/30 p-1 font-mono">
+    <span class="mr-3 w-20 rounded-sm bg-white/10 p-1 font-bold">SHA256</span>
+    <p class="w-full overflow-scroll rounded-sm bg-white/10 p-1 font-mono">
       {hashes.sha256}
     </p>
   </div>
