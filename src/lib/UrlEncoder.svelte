4de3200380c729953fea7d3cc77a7762b75c2c62
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-06 21:57:42 +0400

    feat: url encodor/decodor tool

---
+++ src/lib/UrlEncoder.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  let encode_input = "Hello World";
+  let decode_input = "Hello%20World";
+  let encoded_output = "";
+  let decoded_output = "";
+
+  function encodedURI(input: string) {
+    encoded_output = encodeURIComponent(input);
+  }
+
+  function decodedURI(input: string) {
+    decoded_output = decodeURIComponent(input);
+    decoded_output = decodeURIComponent(input);
+  }
+
+  $: encodedURI(encode_input);
+  $: decodedURI(decode_input);
+</script>
+
+<h1 class="mb-3 text-lg font-semibold">Encode</h1>
+<h2>Your string :</h2>
+<textarea
+  bind:value={encode_input}
+  placeholder="Input"
+  class="mb-3 mt-2 h-20 w-full rounded-sm bg-white/10 p-1"
+  name="input"
+  id="input"
+></textarea>
+
+<h2 class="mb-2 text-lg">Your string encoded :</h2>
+<textarea
+  readonly
+  placeholder="Output"
+  bind:value={encoded_output}
+  class="h-20 w-full rounded-sm bg-white/10 p-1"
+  name="output"
+  id="output"
+></textarea>
+
+<h1 class="my-3 border-t-2 border-white/10 pt-2 text-lg font-semibold">
+  Decode
+</h1>
+<h2>Your string :</h2>
+<textarea
+  placeholder="Input"
+  bind:value={decode_input}
+  class="mb-3 mt-2 h-20 w-full rounded-sm bg-white/10 p-1"
+  name="input"
+  id="input"
+></textarea>
+
+<h2 class="mb-2 text-lg">Your string encoded :</h2>
+<textarea
+  placeholder="Output"
+  readonly
+  bind:value={decoded_output}
+  class="h-20 w-full rounded-sm bg-white/10 p-1"
+  name="output"
+  id="output"
+></textarea>
