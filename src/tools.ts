916fadc618fa6a1a18a001b97044ec19aeeaaf2d
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-06 20:00:57 +0400

    move: `tools.ts` back one directory

--- src/tools.ts
+++ src/tools.ts
@@ -20,5 +20,12 @@ export const tools: Tools[] = [
 		description: "Hash a text string using the function you need : MD5, SHA1, SHA256",
 		component: "HashText",
 		link: "/hash-text"
+	},
+	{
+		title: "Encode/decode url formatted strings",
+		icon_name: "oui:link",
+		description: "Encode to url-encoded format (also known as 'percent-encoded') or decode",
+		link: "/url-encoder",
+		component: "UrlEncoder"
 	}
 ]
