c0669a733e2a5bf051f0379b58adce62dd054d67
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-10 21:10:21 +0400

    update: the name of the site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 <title>Tooly</title>
 <nav class="flex items-center gap-2 px-8 py-4">
   <Icon class="text-4xl" icon="solar:box-bold" />
-  <a class="text-4xl font-bold" href="/">Tooly</a>
+  <a class="text-4xl font-bold" href="/">ToolPack</a>
 </nav>
 
 <main class="px-8">
