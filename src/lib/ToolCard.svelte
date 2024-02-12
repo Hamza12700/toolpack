8c931defa0ffd90983e441c3316e41f4a97763c2
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-02-12 22:44:58 +0400

    refactor: using svelte `on:click|preventDefault` instead of manualy calling a function

--- src/lib/ToolCard.svelte
+++ src/lib/ToolCard.svelte
@@ -4,17 +4,13 @@
   export let title: string
   export let description: string
   export let icon_name: string
-
-  function add_to_favorite(event: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement }) {
-    event.preventDefault()
-  }
 </script>
 
 <a class="border-2 border-white/20 rounded-md" data-sveltekit-preload-data href={link}>
   <div class="w-96 h-44 px-4 py-3 rounded-md bg-white/10">
     <div class="flex items-center justify-between">
       <Icon class="text-3xl" icon={icon_name}/>
-      <button on:click={add_to_favorite}>
+      <button on:click|preventDefault>
         <Icon id="like" class="text-lg text-gray-500 hover:text-red-400" icon="mdi:heart" />
       </button>
     </div>
